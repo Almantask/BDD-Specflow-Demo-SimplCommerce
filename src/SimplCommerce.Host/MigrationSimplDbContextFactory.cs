7175f8f5fd723aa0ebf97e4cf591d39f79789ba2
AlmantasK <almantusk@gmail.com>
2022-08-21 20:17:50 +0300

    Updated to all point to WebHost instea of Host due to some weird issue of not finding assembly when applying migration

---
+++ src/SimplCommerce.Host/MigrationSimplDbContextFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimplCommerce.WebHost.Extensions;
+using SimplCommerce.Infrastructure;
+using SimplCommerce.Module.Core.Data;
+
+namespace SimplCommerce.WebHost
+{
+    public class MigrationSimplDbContextFactory : IDesignTimeDbContextFactory<SimplDbContext>
+    {
+        public SimplDbContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var contentRootPath = Directory.GetCurrentDirectory();
+
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(contentRootPath)
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{environmentName}.json", true);
+
+            builder.AddUserSecrets(typeof(MigrationSimplDbContextFactory).Assembly, optional: true);
+            builder.AddEnvironmentVariables();
+            var _configuration = builder.Build();
+
+            IServiceCollection services = new ServiceCollection();
+            GlobalConfiguration.ContentRootPath = contentRootPath;
+            services.AddModules();
+            services.AddCustomizedDataStore(_configuration);
+            var _serviceProvider = services.BuildServiceProvider();
+
+            return _serviceProvider.GetRequiredService<SimplDbContext>();
+        }
+    }
+}
